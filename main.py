c46a6b1b14707f10a2fa4de6eb748e494ee3cb93
garik <igorkireev02@mail.ru>
2025-09-22 13:37:08 +0300

    add test data

--- main.py
+++ main.py
@@ -35,3 +35,25 @@ class TreeStore:
             result.append(parent)
             current_item = parent
         return result
+
+
+items = [
+    {"id": 1, "parent": "root"},
+    {"id": 2, "parent": 1, "type": "test"},
+    {"id": 3, "parent": 1, "type": "test"},
+    {"id": 4, "parent": 2, "type": "test"},
+    {"id": 5, "parent": 2, "type": "test"},
+    {"id": 6, "parent": 2, "type": "test"},
+    {"id": 7, "parent": 4, "type": None},
+    {"id": 8, "parent": 4, "type": None}
+]
+
+
+ts = TreeStore(items)
+
+
+print(ts.getAll())
+print(ts.getItem(7))
+print(ts.getChildren(4))
+print(ts.getChildren(5))
+print(ts.getAllParents(7))
