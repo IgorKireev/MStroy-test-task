efb0e7cc55817507c6dd788865c4df44ff2712d6
garik <igorkireev02@mail.ru>
2025-09-22 13:20:52 +0300

    initial class

---
+++ main.py
@@ -0,0 +1,12 @@
+class TreeStore:
+    def __init__(self, list_items: list):
+        self.list_items = list_items
+        self.items_by_id = {}
+        self.children_by_parent = {}
+        self.build_index()
+
+    def build_index(self):
+        for item in self.list_items:
+            self.items_by_id[item["id"]] = item
+            self.children_by_parent.setdefault(item["parent"], []).append(item)
+
