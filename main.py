ad7e417d6cc94cc3cb64e519e6b7a2827d426fc5
garik <igorkireev02@mail.ru>
2025-09-22 13:31:38 +0300

    add type hints

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 class TreeStore:
-    def __init__(self, list_items: list):
+    def __init__(self, list_items: list) -> None:
         self.list_items = list_items
         self.items_by_id = {}
         self.children_by_parent = {}
         self.build_index()
 
-    def build_index(self):
+    def build_index(self) -> None:
         for item in self.list_items:
             self.items_by_id[item["id"]] = item
             self.children_by_parent.setdefault(item["parent"], []).append(item)
 
-    def getAll(self):
+    def getAll(self) -> list:
         return self.list_items
 
-    def getItem(self, id):
+    def getItem(self, id: int) -> dict:
         return self.items_by_id.get(id)
 
-    def getChildren(self, id):
+    def getChildren(self, id: int) -> list:
         return self.children_by_parent.get(id, [])
 
-    def getAllParents(self, id):
+    def getAllParents(self, id: int) -> list:
         result = []
         current_item = self.items_by_id.get(id)
         while current_item and current_item["parent"] != "root":
